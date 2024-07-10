2dfa4db8335e4156114499cb5f2c56dcb1c6d947
0xpantera <0xpantera@proton.me>
2024-07-10 16:48:17 -0600

    Format HTTP requests to use `TcpStream` directly

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
+use std::{
+    collections::HashSet,
+    io::{self, Read, Result, Write},
+    net::TcpStream,
+    env
+};
+
+use ffi::Event;
+use poll::Poll;
+
 mod ffi;
 mod poll;
 
 fn main() {
     println!("Hello, world!");
+}
+
+fn get_req(path: &str) -> String {
+    format!(
+        "GET {path} HTTP/1.1\r\n\
+             Host: localhost\r\n\
+             Connection: close\r\n\
+             \r\n"
+    )
 }
